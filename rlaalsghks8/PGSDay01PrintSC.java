3e3fa1bbcae249b0d4eeb46edede9551c6b82940
rlaalsghks8 <127469367+rlaalsghks8@users.noreply.github.com>
2023-07-11 21:05:15 +0900

    [PGS]Day01_Solution 설명

--- rlaalsghks8/PGSDay01PrintSC.java
+++ rlaalsghks8/PGSDay01PrintSC.java
@@ -1,5 +1,9 @@
 public class PGSDay01PrintSC{
     public static void main(String[] args) {
         System.out.print("!@#$%^&*(+\\\'\"<>?:;");
+        /**
+         *  \' 또는 \" 처럼 따옴표 바로 앞에 백슬래시를 넣어주어야 한다.
+            마찬가지로, 만약 백슬래시 자체를 출력하고 싶다면 \\로 백슬래시를 두 번 입력해야 한다.
+         */
     }
 }
